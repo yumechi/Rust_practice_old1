c9e86c56a6087746733cd8d3f29d29254ff85af0
yumechi <yumechi0525@gmail.com>
2018-08-26 13:17:20 +0900

    chapter 4-1

--- projects/hello_world/src/main.rs
+++ projects/hello_world/src/main.rs
@@ -1,3 +1,44 @@
+fn samples1() {
+    let x1 = 5;
+    let (x2, y2) = (1, 2);
+    let x3: i32 = 5;
+    let mut x4 = 7;
+    // イミュータブルなので変更不可
+    // x1 = 10;
+    x4 = 57;
+
+    println!("{}", x1);
+    println!("{} {}", x2, y2);
+    println!("{}", x3);
+    println!("{}", x4);
+
+    // 初期化しないとCE
+    // let x: i32;
+    // println!("The value of x is: {}", x);
+
+    let x5: i32 = 17;
+    {
+        let y: i32 = 3;
+        println!("The value of x is {} and value of y is {}", x5, y);
+    }
+    // スコープ外なのでCE
+    // println!("The value of x is {} and value of y is {}", x5, y); // これは動きません
+
+    // シャドーイング
+    {
+        let x: i32 = 8;
+        {
+            println!("{}", x); // "8"をprint
+            let x = 12;
+            println!("{}", x); // "12"をprint
+        }
+        println!("{}", x); // "8"をprint
+        let x = 42;
+        println!("{}", x); // "42"をprint
+    }
+}
+
 fn main() {
     println!("Hello, World!");
+    samples1();
 }
